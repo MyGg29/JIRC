806dc78b5382b82d5d6629694926162aa61a7ec6
Gwendal <gwendalgourmelon@gmail.com>
2018-10-10 10:54:12 +0200

    Tabs, mongo, control socket, commentaires

--- src/Client/Controller.java
+++ src/Client/Controller.java
@@ -2,11 +2,11 @@ package Client;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
 
 import javax.swing.*;
+import java.io.IOException;
 
 
 public class Controller {
@@ -16,15 +16,19 @@ public class Controller {
     private TextArea textArea;
     @FXML
     private Button send;
-
-    private String
-
+    @FXML
+    private TabPane tabs;
     @FXML
     private void sendText(ActionEvent e){
         System.out.println(textInput.getText());
+        int numTabs = tabs.getTabs().size();
+        Tab newTab = new Tab("Tab" + (numTabs + 1));
+        tabs.getTabs().add(newTab);
+        try {
+            tabs.getTabs().addAll((Tab) FXMLLoader.load(this.getClass().getResource("tab.fxml")));
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
     }
 
-    public String getTextInput(){
-        return textInput.getText();
-    }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,26 +1,49 @@
 package Server;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.BSON;
+import org.bson.Document;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class Server {
     public static void main(String[] args) {
         int port = 666; //random port number
+        List clients = new ArrayList<DataOutputStream>(); //liste des clients connectés
+
+        MongoClient mongo = new MongoClient("localhost",27017);
+        MongoDatabase JIRC = mongo.getDatabase("JIRC");
+        MongoCollection logsCollection =JIRC.getCollection("logs");
+        Document message = new Document();
+        message.put("Sec", "second message");
+        logsCollection.insertOne(message);
+
+
+
         try {
-            ServerSocket ss = new ServerSocket(port);
-            System.out.println("Waiting for a client....");
+            ServerSocket ss = new ServerSocket(port); //Le serveur ecoute quel port
+            System.out.println("Ecoute sur le port " + port);
 
-            System.out.println("Got a client :) ... Finally, someone saw me through all the cover!");
-            System.out.println();
             while(true) {
-                Socket socket = ss.accept();
+                Socket socket = ss.accept();//La connexion est faite entre le client (port random) et le serveur (port 666)
 
+                //On ajoute le client dans la liste des clients connectés.
+                clients.add(new DataOutputStream(socket.getOutputStream()));
+                //On crée un thread par client
                 SSocket sSocket = new SSocket(socket);
                 Thread t = new Thread(sSocket);
                 t.start();
-                System.out.println("Socket Stack Size-----");//+socketMap.size());
             }
         }
         catch (Exception e) { }
@@ -37,16 +60,19 @@ class SSocket implements Runnable {
     @Override
     public void run() {
         try {
-            InputStream in = socket.getInputStream();
-            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();//Where we receive
+            OutputStream out = socket.getOutputStream();//Where we write
 
+            //Des abstractions en plus afin que l'on lise des lettres et pas des bytes[]
+            //Sinon in.read() rends les lettres une par une avec des entiers de 0 à 255
             DataInputStream dIn = new DataInputStream(in);
             DataOutputStream dOut = new DataOutputStream(out);
 
             String line = null;
             while (true) {
+                //Les données sont reçu en utf-16
                 line = dIn.readUTF();
-                System.out.println("Recievd the line----" + line);
+                System.out.println("Received from " + socket.getRemoteSocketAddress() + " : " + line);
                 dOut.writeUTF(line + " Comming back from the server");
                 dOut.flush();
                 System.out.println("waiting for the next line....");
